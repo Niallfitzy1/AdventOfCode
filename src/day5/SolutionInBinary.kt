312fca1677240d864286e911ff6523f21abd2ebf
Niall Fitzpatrick <18366490+Niallfitzy1@users.noreply.github.com>
2020-12-06 20:54:01 +0000

    Day 5 but correctly using binary

---
+++ src/day5/SolutionInBinary.kt
@@ -0,0 +1,23 @@
+package day5
+
+import java.io.File
+
+fun main() {
+    val passes = File("src/day5/input").readLines()
+    val seatIds = passes.map { boardingPass ->
+        val asBinaryString = boardingPass
+            .replace('F', '0')
+            .replace('B', '1')
+            .replace('L', '0')
+            .replace('R', '1')
+        Integer.parseInt(asBinaryString, 2)
+    }.sorted()
+    println(seatIds.max())
+    for (seat in seatIds.indices) {
+        val currentSeat = seatIds[seat]
+        if (seatIds[seat + 1] != currentSeat + 1) {
+            println(currentSeat + 1)
+            break
+        }
+    }
+}
